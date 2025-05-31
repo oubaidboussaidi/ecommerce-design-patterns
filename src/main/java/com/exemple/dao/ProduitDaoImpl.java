3626da5c3fe54952adeccab91ce5ef0e239846ce
Oubaid Boussaidi <oubaydboussaidi@gmail.com>
2025-05-31 12:30:49 +0100

    use Singleton HibernateUtil in UserDao and ProduitDaoImpl for centralized session management

--- src/main/java/com/exemple/dao/ProduitDaoImpl.java
+++ src/main/java/com/exemple/dao/ProduitDaoImpl.java
@@ -1,26 +1,16 @@
 package com.exemple.dao;
 
 import com.exemple.model.Produit;
+import com.exemple.util.HibernateUtil;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
 import java.util.List;
 
 public class ProduitDaoImpl implements IProduitDao {
 
-    private static final SessionFactory factory;
-
-    static {
-        factory = new Configuration()
-                .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(Produit.class)
-                .buildSessionFactory();
-    }
-
     @Override
     public Produit save(Produit p) {
-        try (Session session = factory.getCurrentSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
             session.beginTransaction();
             session.save(p);
             session.getTransaction().commit();
@@ -32,7 +22,7 @@ public class ProduitDaoImpl implements IProduitDao {
     @Override
     public Produit getProduit(Long id) {
         Produit produit = null;
-        try (Session session = factory.getCurrentSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
             session.beginTransaction();
             produit = session.get(Produit.class, id);
             session.getTransaction().commit();
@@ -42,7 +32,7 @@ public class ProduitDaoImpl implements IProduitDao {
 
     @Override
     public Produit updateProduit(Produit p) {
-        try (Session session = factory.getCurrentSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
             session.beginTransaction();
             session.update(p);
             session.getTransaction().commit();
@@ -53,7 +43,7 @@ public class ProduitDaoImpl implements IProduitDao {
 
     @Override
     public void deleteProduit(Long id) {
-        try (Session session = factory.getCurrentSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
             session.beginTransaction();
             Produit produit = session.get(Produit.class, id);
             if (produit != null) {
@@ -68,7 +58,7 @@ public class ProduitDaoImpl implements IProduitDao {
 
     public List<Produit> getAllProduits() {
         List<Produit> produits;
-        try (Session session = factory.getCurrentSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
             session.beginTransaction();
             produits = session.createQuery("from Produit", Produit.class).getResultList();
             session.getTransaction().commit();
--- src/main/java/com/exemple/dao/UserDao.java
+++ src/main/java/com/exemple/dao/UserDao.java
@@ -1,29 +1,16 @@
 package com.exemple.dao;
 
 import com.exemple.model.User;
+import com.exemple.util.HibernateUtil;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
 import java.util.List;
 
 public class UserDao {
 
-    private static final SessionFactory factory;
-
-    static {
-        factory = new Configuration()
-                .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(User.class)
-                .buildSessionFactory();
-    }
-
-    public UserDao() {
-    }
-
     public void save(User u) {
-        try (Session session = factory.getCurrentSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
             session.beginTransaction();
             session.save(u);
             session.getTransaction().commit();
@@ -35,16 +22,15 @@ public class UserDao {
 
     public User verifyUser(String login, String password) {
         User verifiedUser = null;
-        try (Session session = factory.getCurrentSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
             session.beginTransaction();
 
             Query<User> query = session.createQuery(
                     "FROM User WHERE login = :login AND motdepasse = :password", User.class);
             query.setParameter("login", login);
-            query.setParameter("password", password); // A hasher dans un vrai contexte sécurisé
+            query.setParameter("password", password);
 
             verifiedUser = query.uniqueResult();
-
             session.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
@@ -53,7 +39,7 @@ public class UserDao {
     }
 
     public boolean isEmailExists(String email) {
-        try (Session session = factory.getCurrentSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
             session.beginTransaction();
 
             Query<Long> query = session.createQuery(
@@ -72,7 +58,7 @@ public class UserDao {
 
     public List<User> getAllUsers() {
         List<User> users = null;
-        try (Session session = factory.getCurrentSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
             session.beginTransaction();
             users = session.createQuery("FROM User", User.class).getResultList();
             session.getTransaction().commit();
@@ -84,7 +70,7 @@ public class UserDao {
 
     public List<User> searchUsersByEmail(String emailFilter) {
         List<User> users = null;
-        try (Session session = factory.getCurrentSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
             session.beginTransaction();
 
             Query<User> query = session.createQuery(
