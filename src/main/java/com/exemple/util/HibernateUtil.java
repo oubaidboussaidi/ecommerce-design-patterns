73f13eb3ede9c41c72e917955c1ab3bf679efe31
Oubaid Boussaidi <oubaydboussaidi@gmail.com>
2025-05-31 12:29:24 +0100

    adding singleton pattern to ensure only 1 session factory instance

--- src/main/java/com/exemple/util/HibernateUtil.java
+++ src/main/java/com/exemple/util/HibernateUtil.java
@@ -1,23 +1,33 @@
 package com.exemple.util;
 
+import com.exemple.model.Produit;
+import com.exemple.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+
+    //eager initialization
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
+    //private constructor to prevent instantiation
+    private HibernateUtil() {}
+
+    //build SessionFactory
     private static SessionFactory buildSessionFactory() {
         try {
             return new Configuration()
                     .configure("hibernate.cfg.xml")
-                    .addAnnotatedClass(com.exemple.model.User.class)
+                    .addAnnotatedClass(User.class)
+                    .addAnnotatedClass(Produit.class)
                     .buildSessionFactory();
         } catch (Throwable ex) {
-            System.err.println("Initial SessionFactory creation failed." + ex);
+            System.err.println("Initial SessionFactory creation failed: " + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
 
+    // public accessor
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
